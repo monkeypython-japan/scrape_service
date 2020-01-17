b1a412d366a3831330a08fc52e539c06d6db7bae
mamoru <mamoru.kobayashi@gmail.com>
2020-01-17 10:41:44 +0900

    Add path for login page as service top page

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
+    path('', LoginView.as_view(),name='top' ),  # Incorrect way
     path('admin/', admin.site.urls),
     path('registration/', include('registration.urls')),
     path('accounts/', include('accounts.urls')),
