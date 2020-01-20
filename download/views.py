716f19ea174036a33c19715bd0c2a00dca3b3453
mamoru <mamoru.kobayashi@gmail.com>
2020-01-20 17:11:29 +0900

    Remove unuse code.

--- download/views.py
+++ download/views.py
@@ -67,25 +67,3 @@ class DownloadView(View):
         for row in values:
             writer.writerow(row)
         return response
-
-    # def render_to_csv(self, data, fieldnames=None, with_bom=True,
-    #                   fail_silent=True):
-    #     response = HttpResponse(content_type=self.content_type)
-    #     response['Content-Disposition'] = self.get_context_disposition()
-    #     if with_bom:
-    #         response.write('\ufeff')
-    #     if fieldnames:
-    #         writer = csv.DictWriter(
-    #             response, fieldnames=fieldnames, quoting=csv.QUOTE_NONNUMERIC)
-    #         writer.writeheader()
-    #     else:
-    #         writer = csv.writer(response, quoting=csv.QUOTE_NONNUMERIC)
-    #     for row in data:
-    #         try:
-    #             writer.writerow(row)
-    #         except:
-    #             if fail_silent:
-    #                 pass
-    #             else:
-    #                 raise
-    #     return response
